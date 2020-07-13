05a42880d0b16f3dc59506a8ae501f3b9fca0521
evgenyrasskazov <rasskazov@vl.ru>
2020-07-13 16:13:05 +0700

    DS-4050: Правки.

--- src/main/java/com/plugin/inspection/type/KotlinValueTypeInspection.kt
+++ src/main/java/com/plugin/inspection/type/KotlinValueTypeInspection.kt
@@ -52,6 +52,8 @@ class KotlinValueTypeInspection : AbstractKotlinInspection() {
                 val firstChild: PsiElement? = expressionOrInitializer?.firstChild
                 val lastChild: PsiElement? = expressionOrInitializer?.lastChild
 
+                println("exprOrInit = $expressionOrInitializer, ${expressionOrInitializer?.text}, firstCh = $firstChild, lastCh = $lastChild")
+
                 when (expressionOrInitializer) {
                     is KtConstantExpression -> {
                         if (lastChild?.text == "null") {
@@ -66,7 +68,11 @@ class KotlinValueTypeInspection : AbstractKotlinInspection() {
                         }
                     }
                     is KtReferenceExpression,
-                    is KtLambdaExpression -> {
+                    is KtLambdaExpression,
+                    is KtBinaryExpression,
+                    is KtDoWhileExpression,
+                    is KtIfExpression,
+                    is KtWhenExpression -> {
                         registerProblem(holder, property)
                         return
                     }
