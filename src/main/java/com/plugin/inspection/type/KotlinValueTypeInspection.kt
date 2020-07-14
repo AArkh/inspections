6878a40cbd00e17a5a629732b80f202dce4db806
evgenyrasskazov <rasskazov@vl.ru>
2020-07-14 17:01:33 +0700

    DS-4050: Правки.

--- src/main/java/com/plugin/inspection/type/KotlinValueTypeInspection.kt
+++ src/main/java/com/plugin/inspection/type/KotlinValueTypeInspection.kt
@@ -11,6 +11,7 @@ import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.nj2k.postProcessing.type
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.prevLeaf
 
 private const val DISPLAY_NAME = "The value without type declaration"
 
@@ -52,6 +53,12 @@ class KotlinValueTypeInspection : AbstractKotlinInspection() {
                 val firstChild: PsiElement? = expressionOrInitializer?.firstChild
                 val lastChild: PsiElement? = expressionOrInitializer?.lastChild
 
+                println("prop = ${property.name}, " +
+                        "exp = $expressionOrInitializer, ${expressionOrInitializer?.text}, " +
+                        "fCh = $firstChild, ${firstChild?.text}, " +
+                        "lCh = $lastChild, ${lastChild?.text}"
+                )
+
                 when (expressionOrInitializer) {
                     is KtConstantExpression -> {
                         if (lastChild?.text == "null") {
@@ -75,10 +82,6 @@ class KotlinValueTypeInspection : AbstractKotlinInspection() {
                         return
                     }
                     is KtDotQualifiedExpression -> {
-                        if (lastChild == firstChild && lastChild is KtLambdaArgument) {
-                            registerProblem(holder, property)
-                            return
-                        }
                         if (isMethodCall(expressionOrInitializer)) {
                             registerProblem(holder, property)
                             return
@@ -104,27 +107,28 @@ class KotlinValueTypeInspection : AbstractKotlinInspection() {
     }
 
     private fun isMethodCall(expressionOrInitializer: PsiElement): Boolean {
-        val children: Array<PsiElement> = expressionOrInitializer.children
-        var lastChild: PsiElement? = expressionOrInitializer.lastChild
-        var preLastChild: PsiElement? = if (children.size > 1) children[children.size - 2] else null
+        val lastChild: PsiElement? = expressionOrInitializer.lastChild
+        val preLastChild: PsiElement? = lastChild?.prevLeaf(false)
 
-        // Разбираем lastChild до деталей.
-        while (lastChild is KtCallExpression) {
-            val lastChildChildren: Array<PsiElement> = lastChild.children
-            preLastChild = if (lastChildChildren.size > 1) {
-                lastChildChildren[lastChildChildren.lastIndex.dec()]
-            } else null
-            lastChild = lastChild.lastChild
+        // Проверка на нижний регистр для понимания, метод это или создание объекта.
+        return when (lastChild) {
+            // Если lastChild - это KtValueArgumentList (скобочки с аргументами),
+            // то проверяем на регистр предыдущего ребенка - [название метода/класса].
+            is KtValueArgumentList -> {
+                preLastChild?.text
+                        ?.first()
+                        ?.isLowerCase()
+                        ?: false
+            }
+            // Если lastChild - это KtCallExpression, то проверяем на регистр его, т.к. KtCallExpression - это элемент,
+            // представленный в формате: "[название метода/класса]()"
+            is KtCallExpression -> {
+                lastChild.text
+                        .first()
+                        .isLowerCase()
+            }
+            else -> false
         }
-
-        return if (lastChild is KtValueArgumentList) {
-            // Проверка на нижний регистр для понимания, метод это или конструктор.
-            preLastChild?.text
-                    ?.first()
-                    ?.isLowerCase()
-                    ?: false
-        } else false
-
     }
 }
 
